b856b696b638908e51c3d87a1cace15ed9b371b9
Mike Green <greens1593@gmail.com>
2022-09-29 19:52:40 +0300

    checkRoleMiddleware was created and added to brand, device and type routers

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const brandController = require('../controllers/brandController');
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
 const router = new Router();
 
-router.post('/', brandController.create)
+router.post('/', checkRoleMiddleware, brandController.create)
 router.get('/', brandController.getAll)
 
 
--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,12 +1,13 @@
 const Router = require('express');
 const deviceController = require('../controllers/deviceController');
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
 const router = new Router();
 
-router.post('/', deviceController.create)
+router.post('/', checkRoleMiddleware, deviceController.create)
 router.get('/', deviceController.getAll)
 router.get('/:id', deviceController.getOne)
-router.delete('/:id', deviceController.delete)
+router.delete('/:id', checkRoleMiddleware, deviceController.delete)
 
 
 module.exports = router
