85c582db4c233c7724beb569b9558f24de6ba23c
Mike Green <greens1593@gmail.com>
2022-10-01 13:14:27 +0300

    Styles of NavBar was changed

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../index";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Button} from 'react-bootstrap';
+import {Button, Container} from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
 import { SHOP_ROUTE } from "../utils/consts";
 import { observer } from "mobx-react-lite";
@@ -10,18 +10,20 @@ import { observer } from "mobx-react-lite";
 const NavBar = observer ( () => {
     const {user} = useContext(Context)
     return (
-        <Navbar style={{display:'flex', padding:'20px', justifyContent:'space-between', color:'white'}} bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark">
+        <Container>
           <NavLink style={{color:'white', textDecoration:'none'}} to={SHOP_ROUTE}>Store</NavLink>
           {user.isAuth ?
-          <Nav style={{color:'white'}} className="mr-auto">
-                <Button variant='outline-light'>Админпанель</Button>
-                <Button style={{marginLeft:'4%'}} variant='outline-light' onClick={()=>user.setIsAuth(false)}>Выйти</Button>
+            <Nav style={{color:'white'}} className="ml-auto">
+                <Button variant={'outline-light'}>Админпанель</Button>
+                <Button variant={'outline-light'} style={{marginLeft:'10px'}} onClick={()=>user.setIsAuth(false)}>Выйти</Button>
           </Nav>
           :
-          <Nav style={{color:'white'}} className="mr-auto">
-                <Button variant='outline-light' onClick={()=>user.setIsAuth(true)}>Авторизация</Button>
+          <Nav style={{color:'white'}} className="ml-auto">
+                <Button variant={'outline-light'} onClick={()=>user.setIsAuth(true)}>Авторизация</Button>
           </Nav>
           }
+        </Container>
         </Navbar>
     )
 })
