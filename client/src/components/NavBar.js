c2712ba8a0eba2158ce3d4524a0ff80731cc36e5
Mike Green <greens1593@gmail.com>
2022-10-02 21:49:24 +0300

    Admin page was created. Modals createType, createBrand, createDevice was created and adeed to adminPage

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,24 +3,38 @@ import { Context } from "../index";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Button, Container} from 'react-bootstrap';
-import { NavLink } from "react-router-dom";
-import { SHOP_ROUTE } from "../utils/consts";
+import { NavLink, useHistory } from "react-router-dom";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import { observer } from "mobx-react-lite";
 
 const NavBar = observer ( () => {
     const {user} = useContext(Context)
+    const history = useHistory()
     return (
       <Navbar bg="dark" variant="dark">
         <Container>
           <NavLink style={{color:'white', textDecoration:'none'}} to={SHOP_ROUTE}>Store</NavLink>
           {user.isAuth ?
             <Nav style={{color:'white'}} className="ml-auto">
-                <Button variant={'outline-light'}>Админпанель</Button>
-                <Button variant={'outline-light'} style={{marginLeft:'10px'}} onClick={()=>user.setIsAuth(false)}>Выйти</Button>
+                <Button 
+                  variant={'outline-light'}
+                  onClick={()=>history.push(ADMIN_ROUTE)}>
+                    Админпанель
+                </Button>
+                <Button 
+                  variant={'outline-light'} 
+                  style={{marginLeft:'10px'}} 
+                  onClick={()=>history.push(LOGIN_ROUTE)}>
+                    Выйти
+                </Button>
           </Nav>
           :
           <Nav style={{color:'white'}} className="ml-auto">
-                <Button variant={'outline-light'} onClick={()=>user.setIsAuth(true)}>Авторизация</Button>
+                <Button 
+                  variant={'outline-light'} 
+                  onClick={()=>user.setIsAuth(true)}>
+                    Авторизация
+                </Button>
           </Nav>
           }
         </Container>
