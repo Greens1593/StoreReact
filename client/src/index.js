9c1fdf6762465e2d30ab28634fd7336120ff4a52
Mike Green <greens1593@gmail.com>
2022-10-03 23:25:32 +0300

    Index.js was changet to React 18 build. Axios queries was started to create.

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,18 @@
 import React, { createContext } from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 import DeviceStore from './store/DeviceStore';
 import UserStore from './store/UserStore';
 
 export const Context = createContext(null)
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
     <Context.Provider value={{
         user: new UserStore(),
         device: new DeviceStore(),
     }}>
         <App />
     </Context.Provider>,
-    document.getElementById('root')
 );
